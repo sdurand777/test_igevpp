eacf0c804d4de582bb5ed903a8f9a14b5bbbb918
ivm <stan777.durand777@gmail.com>
2024-10-28 17:19:06 +0100

    WIP

---
+++ commande_igevpp.sh
@@ -0,0 +1,1 @@
+python demo_imgs.py --restore_ckpt igevpp.pth --left_imgs './demo-imgs/*/im0.png' --right_imgs './demo-imgs/*/im1.png'
---
+++ commande_real_time.sh
@@ -0,0 +1,1 @@
+python demo_imgs_rt.py --restore_ckpt igevpp_rt.pth --left_imgs './demo-imgs/*/im0.png' --right_imgs './demo-imgs/*/im1.png'
